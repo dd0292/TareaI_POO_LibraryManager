a315f5124faf93e1960557d49cae72abc7eade1c
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-08-31 16:04:14 -0600

    Package

---
+++ SistemaGestionBiblioteca/src/main/java/bracr10/sistemagestionbiblioteca/SistemaGestionBiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bracr10.sistemagestionbiblioteca;
+
+/**
+ *
+ * @author Brian Ramirez
+ */
+public class SistemaGestionBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
